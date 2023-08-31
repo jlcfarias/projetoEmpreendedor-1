db78ac51c021e759827b0cf7c320f61eaac57069
1903larissaalves@gmail.com <1903larissaalves@gmail.com>
2023-08-31 13:04:12 -0300

    inserido voltar nas telas de imc e contador de calorias

--- TreinoDieta/app/src/main/java/com/larissa/treinodieta/CalculoImcActivity.java
+++ TreinoDieta/app/src/main/java/com/larissa/treinodieta/CalculoImcActivity.java
@@ -1,7 +1,11 @@
 package com.larissa.treinodieta;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +18,23 @@ public class CalculoImcActivity extends AppCompatActivity {
     private EditText editPeso;
     private EditText editAltura;
     private Button btnlimpar;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mMenuInflater =  getMenuInflater();
+        mMenuInflater.inflate(R.menu.voltar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_voltar) {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
