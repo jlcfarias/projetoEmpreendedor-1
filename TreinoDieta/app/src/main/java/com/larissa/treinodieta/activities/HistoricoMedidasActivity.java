4392805e1d31f4a2980c91164f5862f51ecfe5cd
1903larissaalves@gmail.com <1903larissaalves@gmail.com>
2023-09-03 17:59:17 -0300

    inserido layout de historico de medidas

---
+++ TreinoDieta/app/src/main/java/com/larissa/treinodieta/activities/HistoricoMedidasActivity.java
@@ -0,0 +1,17 @@
+package com.larissa.treinodieta.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.larissa.treinodieta.R;
+
+public class HistoricoMedidasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historico_medidas);
+        setTitle(R.string.historicoMedidas);
+    }
+
+}
